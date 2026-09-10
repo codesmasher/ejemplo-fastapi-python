6daeeba92152511e0d8276f09f50e2c5ea32e0d4
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2026-09-10 12:54:22 -0600

    feat: Configuracion de DebugPy y VS Code

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+# Configurar el depurador de Python
+import debugpy
+if not debugpy.is_client_connected():
+    try:
+        debugpy.listen(("0.0.0.0", 5678))
+        print("🐍 [DEBUGGER] Escuchando en 0.0.0.0:5678 - Listo para adjuntar VS Code")
+    except RuntimeError:
+        # El puerto ya está en uso por el proceso padre de Uvicorn, continuamos de forma segura
+        pass
+
+app = FastAPI(title="Nombre del sistema")
 
 # Declaración del documento raíz
 @app.get("/")
